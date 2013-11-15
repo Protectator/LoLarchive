6cc5341de6d705c34a1bdf7b007316f29ba79d17
Kewin Dousse <kewin.d@websud.ch>
2013-11-15 16:55:23 +0100

    Updating jQuery and moving it to libs/jQuery/

--- includes/footer.php
+++ includes/footer.php
@@ -3,8 +3,8 @@
 		<p><a href="http://twitter.com/Protectator">Protectator</a></p>
 	  </footer>
     </div>
-	<script src="js/jquery-1.10.0.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+	<script src="libs/jQuery/jquery-1.10.0.min.js"></script>
+	<script src="libs/bootstrap/docs/assets/js/bootstrap.min.js"></script>
 	<script src="js/script.js"></script>
   </body>
 </html>
