587b72ae302a829f4a30734abfddaa43a0c06649
Kewin Dousse <kewin.d@websud.ch>
2014-05-12 17:01:17 +0200

    Replacing bootstrap submodule with compiled

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<!-- Bootstrap -->
-		<link href="<?echo PATH;?>libs/bootstrap/docs/assets/css/bootstrap.css" rel="stylesheet" media="screen">
+		<link href="<?echo PATH;?>libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 		<link href="<?echo PATH;?>libs/darkstrap/darkstrap.min.css" rel="stylesheet" media="screen">
 		<link href="<?echo PATH;?>libs/bootstrap-datepicker/css/datepicker.css" rel="stylesheet" media="screen">
 		<link href="<?echo PATH;?>css/style.css" rel="stylesheet" media="screen">
