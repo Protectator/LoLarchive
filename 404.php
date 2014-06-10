8675f66f0bf23937d836d3987fbdde0804d28b8d
Kewin Dousse <kewin.d@websud.ch>
2014-06-10 04:09:03 +0200

    Licence update

--- 404.php
+++ 404.php
@@ -1,4 +1,27 @@
 <?php
+
+/*	LoLarchive - Website to keeps track of your games in League of Legends
+    Copyright (C) 2013-2014  Kewin Dousse (Protectator)
+
+    This file is part of LoLarchive.
+
+    LoLarchive is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    To contact me : kewin.d@websud.ch
+    Project's repository : https://github.com/Protectator/LoLarchive
+*/
+
 echoHeader("404 error - LoLarchive");
 ?>
 <script>
