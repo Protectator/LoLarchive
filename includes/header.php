c2923e5fda5459a92652abfd4412d64a801c94cd
Kewin Dousse <kewin.d@websud.ch>
2014-06-10 14:27:28 +0200

    Updating licence to AGPL

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,29 @@
 <!DOCTYPE html>
 <html lang="fr">
+<!--
+	This page was generated using LoLarchive.
+	
+	LoLarchive - Website to keep track of your games in League of Legends
+    Copyright (C) 2013-2014  Kewin Dousse (Protectator)
+
+    This file is part of LoLarchive.
+
+    LoLarchive is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or any later version.
+
+    LoLarchive is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact : kewin.d@websud.ch
+    Project's repository : https://github.com/Protectator/LoLarchive
+-->
 	<head>
 		<title><?php echo $title ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
