725815f27c5dfd4722f309e9696f7ca6047bc78a
Kewin Dousse <kewin.d@websud.ch>
2014-08-04 13:08:12 +0200

    Correcting some echoes

--- includes/footer.php
+++ includes/footer.php
@@ -3,12 +3,12 @@
 			<p><a href="https://github.com/Protectator/LoLarchive">LoLarchive</a> © 2013-2014 <a href="http://twitter.com/Protectator">Kewin Dousse</a></p>
 		</footer>
     </div>
-		<script src="<?echo PATH;?>libs/jQuery/jquery-2.1.1.min.js"></script>
-		<script src="<?echo PATH;?>libs/bootstrap/js/bootstrap.min.js"></script>
+		<script src="<?php echo PATH;?>libs/jQuery/jquery-2.1.1.min.js"></script>
+		<script src="<?php echo PATH;?>libs/bootstrap/js/bootstrap.min.js"></script>
 		<script type="text/javascript">
 			today = "<?php echo date('j/n/Y'); ?>";
 		</script>
-		<script src="<?echo PATH;?>libs/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
-		<script src="<?echo PATH;?>js/script.js"></script>
+		<script src="<?php echo PATH;?>libs/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+		<script src="<?php echo PATH;?>js/script.js"></script>
 	</body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -28,11 +28,11 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<!-- Bootstrap -->
-		<link href="<?echo PATH;?>libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-		<link href="<?echo PATH;?>libs/darkstrap/darkstrap.min.css" rel="stylesheet" media="screen">
-		<link href="<?echo PATH;?>libs/bootstrap-datepicker/css/datepicker.css" rel="stylesheet" media="screen">
-		<link href="<?echo PATH;?>css/style.css" rel="stylesheet" media="screen">
-		<link rel="icon" href="<?echo PATH;?>favicon.ico" />
+		<link href="<?php echo PATH;?>libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href="<?php echo PATH;?>libs/darkstrap/darkstrap.min.css" rel="stylesheet" media="screen">
+		<link href="<?php echo PATH;?>libs/bootstrap-datepicker/css/datepicker.css" rel="stylesheet" media="screen">
+		<link href="<?php echo PATH;?>css/style.css" rel="stylesheet" media="screen">
+		<link rel="icon" href="<?php echo PATH;?>favicon.ico" />
 	</head>
 	<body>
 		<?php include_once("analyticstracking.php") ?>
@@ -40,7 +40,7 @@
 			<div class="row">
 				<div class="span12">
 					<div class="hero-unit">
-						<h1 id="title"><a href="<?echo PATH;?>" style="color:inherit;">LoLarchive</a></h1>
+						<h1 id="title"><a href="<?php echo PATH;?>" style="color:inherit;">LoLarchive</a></h1>
 						<h3>League of Legends games archive</h3>
 					</div>
 				</div>
